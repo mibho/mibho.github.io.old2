30755c0f52530718a64e785ee31f118ce8134d94
mibho <86342821+mibho@users.noreply.github.com>
2021-12-01 20:42:13 -0800

    Update windowstuff.js

--- assets/js/windowstuff.js
+++ assets/js/windowstuff.js
@@ -1,8 +1,8 @@
-function getRandomArbitrary(min, max) {
+function getRandomArbitrary2(min, max) {
   return Math.random() * (max - min) + min;
 }
 var hidden = false;
 var cnt = 0;
-var dur = getRandomArbitrary(3,15) * 1000;
+var dur = getRandomArbitrary2(3,15) * 1000;
 
 window.addEventListener('load', visible(cnt, dur));
